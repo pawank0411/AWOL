68dbdff8ce814bae899846a6be3b43b52b6b8c0a
Pawan0411 <pawankumar17264@gmail.com>
2020-07-15 01:27:49 +0530

    fixed errors

--- app/src/main/java/codex/codex_iter/www/awol/activity/AboutActivity.kt
+++ app/src/main/java/codex/codex_iter/www/awol/activity/AboutActivity.kt
@@ -1,14 +1,16 @@
 package codex.codex_iter.www.awol.activity
 
 import android.content.Intent
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
 import android.graphics.PorterDuff
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.MenuItem
-import android.view.View
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import codex.codex_iter.www.awol.R
-import java.util.*
 
 class AboutActivity : BaseThemedActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,25 +18,41 @@ class AboutActivity : BaseThemedActivity() {
         setContentView(R.layout.activity_about)
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
-        title = "About"
-        Objects.requireNonNull(supportActionBar)!!.setDisplayHomeAsUpEnabled(true)
-        Objects.requireNonNull(supportActionBar)!!.elevation = 0f
-        Objects.requireNonNull(supportActionBar).setDisplayShowHomeEnabled(true)
+        supportActionBar?.apply {
+            title = "About"
+            setDisplayHomeAsUpEnabled(true)
+            elevation = 0f
+            setDisplayShowHomeEnabled(true)
+        }
+
         if (!dark) {
-            toolbar.setTitleTextColor(resources.getColor(R.color.black))
-            Objects.requireNonNull(toolbar.navigationIcon).setColorFilter(resources.getColor(R.color.black), PorterDuff.Mode.SRC_ATOP)
+                myDrawableCompact()
         }
     }
 
-    fun openFacebook(view: View?) {
+    private fun myDrawableCompact() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+            findViewById<Toolbar>(R.id.toolbar)?.apply {
+                setTitleTextColor(ContextCompat.getColor(context,R.color.black))
+                navigationIcon?.colorFilter = BlendModeColorFilter(ContextCompat.getColor(context,R.color.black),
+                        BlendMode.SRC_ATOP)
+            }
+        } else {
+            findViewById<Toolbar>(R.id.toolbar)?.apply {
+                setTitleTextColor(ContextCompat.getColor(context,R.color.black))
+                navigationIcon?.setColorFilter(ContextCompat.getColor(context,R.color.black), PorterDuff.Mode.SRC_ATOP)
+            }
+        }
+    }
+    fun openFacebook() {
         startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/codexiter/?ref=br_rs")))
     }
 
-    fun openInstagram(view: View?) {
+    fun openInstagram() {
         startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://instagram.com/codexiter?igshid=w8g2cfygo8sy")))
     }
 
-    fun openGmail(view: View?) {
+    fun openGmail() {
         val emailIntent = Intent(Intent.ACTION_SENDTO, Uri.fromParts(
                 "mailto", "codexiter@gmail.com", null))
         emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Feedback for AWOL")
@@ -42,7 +60,7 @@ class AboutActivity : BaseThemedActivity() {
         startActivity(Intent.createChooser(emailIntent, null))
     }
 
-    fun openGithub(view: View?) {
+    fun openGithub() {
         startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/codex-iter/AWOL")))
     }
 
